0eb222c0dae8b97df402119a67f5b368499f3f22
memirkocak <mekocak@edenschool.fr>
2024-03-05 14:20:45 +0100

    v3

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-const menuBurger = document.querySelector('#menuburger');
-const closeBurger = document.querySelector('#closeburger');
-const div = document.querySelector('#div');
+let btnBurger = document.querySelector('#btn-burger');
+let menu = document.querySelector('#divmenu');
+let closebtn = document.querySelector('#close');
+
+btnBurger.addEventListener('click',()=>{
+    menu.style.display = 'block';
+});
+
+closebtn.addEventListener('click', ()=>{
+    menu.style.display = 'none';
+});
